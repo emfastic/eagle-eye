bdd8acca251eb82ee0b364562d04a54c81913e31
Jake Ottiger <ottigerjake@gmail.com>
2023-03-21 21:25:37 -0400

    fixed image?

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -12,16 +12,14 @@ const CustomHeader = () => {
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔎</text></svg>"
       />
-      <meta property="og:image" content="https://eaglesearch.org/image.svg" />
+      <meta property="og:image" content="/image.png" />
       <meta
         property="og:image:alt"
         content="Eagle Eye: Explore Courses and Get Notified (in a red gradient)"
       />
-      <meta
-        name="twitter:card"
-        content="https://eaglesearch.org/twitter_card.svg"
-      />
+      <meta name="twitter:card" content="/image.png" />
       <meta name="twitter:title" content="Eagle Eye" />
+      <meta name="twitter:site" content="@jakeottiger" />
       <meta
         name="twitter:description"
         content="Easily find Boston College courses and get notified when seats open."
