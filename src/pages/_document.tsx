10737150e80a4b3cdb19cb7250d33fe4b68b6540
Jake Ottiger <ottigerjake@gmail.com>
2023-03-21 21:10:06 -0400

    fixed head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import CustomHeader from "../../components/CustomHeader";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <CustomHeader />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
