0bf556684c7a3ac5d0b86bbc9d8702e137464263
Jake Ottiger <ottigerjake@gmail.com>
2023-03-22 10:16:26 -0400

    last attempt at twitter card

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -15,7 +15,10 @@ const CustomHeader = () => {
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔎</text></svg>"
       />
       <meta property="og:image" content="/image.png" />
-      <meta name="twitter:image" content="https://www.eaglesearch.org/twitter_image.png" />
+      <meta
+        name="twitter:image"
+        content="https://www.eaglesearch.org/twitter_card.png"
+      />
       <meta
         property="og:image:alt"
         content="Eagle Eye: Explore Courses and Get Notified (in a red gradient)"
